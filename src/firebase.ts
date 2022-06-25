4a08d04a744afc273f887ca8bca56bd1163e7745
hosnipogi <hosnibona@gmail.com>
2022-06-25 11:12:21 +0800

    heroku config fix

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,10 +1,14 @@
 import * as admin from 'firebase-admin'
-import fbAccountCreds from '../service-account.json'
+// import fbAccountCreds from '../service-account.json'
+// const serviceAccount = fbAccountCreds as admin.ServiceAccount
 
-const serviceAccount = fbAccountCreds as admin.ServiceAccount
+require('dotenv').config()
+const GOOGLE_CONFIG = process.env.GOOGLE_CONFIG
+
+const buffer = Buffer.from(GOOGLE_CONFIG, 'base64') as any
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(JSON.parse(buffer)),
 })
 
 export default admin
